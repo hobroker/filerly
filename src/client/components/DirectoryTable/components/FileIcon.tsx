0440e76fa96df91d5a9471fd9b945eca7bd3d263
Igor <4361921+hobroker@users.noreply.github.com>
2023-04-08 17:59:11 +0300

    add file icons

--- src/client/components/DirectoryTable/components/FileIcon.tsx
+++ src/client/components/DirectoryTable/components/FileIcon.tsx
@@ -1,33 +1,16 @@
-import {
-  type Icon,
-  Folder,
-  File,
-  FileJs,
-  FileTs,
-  FileJsx,
-  FileTsx,
-} from "@phosphor-icons/react";
+import { Folder } from "@phosphor-icons/react";
+import { getIcon } from "material-file-icons";
 
 interface Props {
   isDirectory: boolean;
   filename: string;
 }
 
-const EXTENSION_ICONS: Record<string, Icon> = {
-  js: FileJs,
-  mjs: FileJs,
-  cjs: FileJs,
-  jsx: FileJsx,
-  ts: FileTs,
-  tsx: FileTsx,
-};
-
 export const FileIcon = ({ isDirectory, filename }: Props) => {
   if (isDirectory) {
     return <Folder weight="fill" size={16} />;
   }
-  const extension = filename.match(/\.[0-9a-z]+$/i)?.[0]?.replace(".", "");
-  const Icon = (extension && EXTENSION_ICONS[extension]) || File;
+  const { svg } = getIcon(filename);
 
-  return <Icon size={16} />;
+  return <div className="w-4" dangerouslySetInnerHTML={{ __html: svg }} />;
 };
